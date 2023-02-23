8f03c7d3594f2367513b24dca03d3e19d090064d
Derkach Oleksandr <derkach03553@gmail.com>
2023-02-24 00:22:08 +0200

    add admin panel

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from task_manager.models import Worker, Position, TaskType, Task
+
+admin.site.register(Position)
+admin.site.register(TaskType)
+admin.site.register(Task)
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position", )
