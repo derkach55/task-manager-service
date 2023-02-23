1d14ef17c630dc379305cae7ebb55c7caf9d171a
Derkach Oleksandr <derkach03553@gmail.com>
2023-02-23 23:50:39 +0200

    add db models and migrations

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,42 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.username}({self.first_name} {self.last_name}): {self.position}"
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = (
+        ('Urgent', 'Urgent Priority'),
+        ('Low', 'Low Priority'),
+        ('Medium', 'Medium Priority'),
+        ('High', 'High Priority'),
+    )
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES, default='Medium')
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name='tasks')
+
+    def __str__(self):
+        return self.name
--- task_manager_service/settings.py
+++ task_manager_service/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+AUTH_USER_MODEL = 'task_manager.Worker'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'task_manager',
 ]
 
 MIDDLEWARE = [
