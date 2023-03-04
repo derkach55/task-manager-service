85a6b42afdfe5a5b7a8e3802a722bbc74382521c
Derkach Oleksandr <derkach03553@gmail.com>
2023-03-04 12:19:24 +0200

    add task-manager-8gy9.onrender.com to ALLOWED_HOSTS

--- task_manager_service/settings.py
+++ task_manager_service/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'task-manager-8gy9.onrender.com']
 
 # Application definition
 AUTH_USER_MODEL = 'task_manager.Worker'
