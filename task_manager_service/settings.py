f9f81f1e1e7105696372d39c7ebc198a37f4a807
Derkach Oleksandr <derkach03553@gmail.com>
2023-03-04 11:56:09 +0200

    remove insecure secret key

--- task_manager_service/settings.py
+++ task_manager_service/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-3j2ds1*yadr*y8lqqq4-a#c95yof3fd9@6s+2m#j!biebv+xa1')
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
